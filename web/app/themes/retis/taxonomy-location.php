b614777519b2506aa1ac9cb06566faf64be75829
ricardocotillo <ricardo.cotillo@gmail.com>
2023-04-24 18:36:19 -0500

    add filter

--- web/app/themes/retis/taxonomy-location.php
+++ web/app/themes/retis/taxonomy-location.php
@@ -4,8 +4,19 @@ use Timber\Timber;
 use Timber\PostQuery;
 
 $templates = array( 'archive-listing.twig' );
-
 $context = Timber::context();
-$context['posts'] = new PostQuery();
+
+if (isset($_GET['type']) && $_GET['type'] != '') {
+    $listing_cat = $_GET['type'];
+    $context['posts'] = new PostQuery([
+        'tax_query' => [
+            [
+                'taxonomy'  => 'listing_cat',
+                'field'     => 'slug',
+                'terms'     => $listing_cat,
+            ],
+        ]
+    ]);
+}
 
 Timber::render( $templates, $context );
